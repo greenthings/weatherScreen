9d4e16d68070f7b6ed894dcb0ed02470a802b586
Jun Shin <69499549+greenthings@users.noreply.github.com>
2021-11-19 21:26:34 +0900

    Add files via upload

---
+++ weatherforecast/weatherforecast/weatherforecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherforecastApp.swift
+//  weatherforecast
+//
+//  Created by greenthings on 2021/11/19.
+//
+
+import SwiftUI
+
+@main
+struct weatherforecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
